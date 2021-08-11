66c97e2452fa37187292807fc19aaee4d1bd4c85
Vanessa <vkwacko007@gmail.com>
2021-08-11 11:25:41 +0100

    Move files from selected runs

--- open_file.R
+++ open_file.R
@@ -21,14 +21,28 @@ open_file <- function(r0_strain1, transmiss, crossimm, preinf, seed, vacc, vacc_
 ### Run function
 output_folder <- "seir_model_output"
 
-file_ending <- "calc.csv"
+file_ending <- "plot_full.png"
 
-r0_strain1 <- 4
-transmiss <- 1
+r0_strain1 <- 1.5
+transmiss <- 1.06
 crossimm <- 1
 preinf <- 2.5
-seed <- 1200
-vacc <- 0
-vacc_start <- 0
+seed <- 301.16
+vacc <- 1
+vacc_start <- 0.1111
 
 open_file(r0_strain1, transmiss, crossimm, preinf, seed, vacc, vacc_start, file_ending, output_folder)
+
+# Move selected runs to Archive
+selected_scenarios <- str_c("^", run_longer$scenario)
+
+output_files <- list.files(file.path(".", output_folder))
+
+files_required <- map(selected_scenarios, str_subset, string = output_files) %>%  unlist(use.names=F)
+
+move_file <- function(x) {
+  file.rename(from = file.path(".", output_folder, x),
+              to = file.path(".", "Archive", "seir_model_output_cutshort", x)) 
+  }
+
+lapply(files_required, move_file)
